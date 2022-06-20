08e001e1e655ebd1213a018f95f06c8329358cf1
ecourse <smkang@codenuri.co.kr>
2022-06-20 11:44:27 +0900

    Add files via upload

--- DAY1/5_range_for4.cpp
+++ DAY1/5_range_for4.cpp
@@ -11,16 +11,29 @@ public:
 	take_view(std::vector<int>& v, int c) : cont(v), count(c) {}
 
 	auto begin() { return cont.begin(); }
-	auto end()   { return cont.end(); }
+	auto end()   { return cont.begin()+count; }
 };
+
+class reverse_view
+{
+	std::vector<int>& cont;
+public:
+	reverse_view(std::vector<int>& v) : cont(v) {}
+
+	auto begin() { return cont.rbegin(); }
+	auto end() { return cont.rend(); };
+};
+
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
 
 	take_view tv(v, 3);
 
-	for (auto e : tv)
+//	for (auto e : tv)
+//	for (auto e : take_view(v, 3))  // 임시객체만들어서 사용(내일 1교시)
+	for (auto e : reverse_view(v))  
 		std::cout << e << std::endl;
-
-
 }
+
+
