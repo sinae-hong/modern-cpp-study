c7b00937724db430f796a5d740459997f53351f1
ecourse <smkang@codenuri.co.kr>
2022-06-20 16:30:54 +0900

    Add files via upload

--- DAY1/14_override2.cpp
+++ DAY1/14_override2.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 
+const int c1 = 10;
+int const c2 = 20;
+
 template<typename T>
 class Base
 {
 public:
-	virtual void foo(const T arg)
-	{
-		std::cout << "Base foo" << std::endl;
-	}
+	// 아래 인자는 _arg_ 가 const 입니다.
+	virtual void foo(const T arg) {	std::cout << "Base foo" << std::endl;	}
+
+	virtual void goo(T const arg) { std::cout << "Base goo" << std::endl; }
+	
+	// 아래 2줄은 동일합니다
+	// const T arg  => 이렇게 많이 사용하는데
+	// T const arg  => 이 코드를 주장하는 사람도 많습니다.
 };
+
 
 class Derived : public Base<int*>
 {
 public:
+	// 아래 코드는 _arg_ 는 const 아닙니다.
+	// 아래 코드는 _arg_ 를 따라가면 const 입니다
+	//virtual void foo(const int* arg) // override 
+
+	// 아래 코드가 _arg_ 가 const 입니다
+	virtual void foo(int* const arg)  override 
+	{
+		std::cout << "Derived foo" << std::endl;
+	}
+
+	virtual void goo(int* const arg) { std::cout << "Derived goo" << std::endl; }
+
 };
+
 int main()
 {
 	Base<int*>* p = new Derived;
