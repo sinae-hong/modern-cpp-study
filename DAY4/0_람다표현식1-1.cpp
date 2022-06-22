f8e3ba5f8c3c4add26148b79d9f43c26f2d50a1d
ecourse <smkang@codenuri.co.kr>
2022-06-23 08:40:42 +0900

    Add files via upload

---
+++ DAY4/0_람다표현식1-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// 0 ~ 9 사이의 난수를 반환 하는 함수 (rand() 사용하세요)
+int urand()
+{
+	return ? ;
+}
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << urand() << std::endl;
+	}
+}
