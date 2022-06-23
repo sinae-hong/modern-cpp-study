00b4d5a103af17f9f044cfd551d2fcdb05be15ef
ecourse <smkang@codenuri.co.kr>
2022-06-23 15:42:02 +0900

    Add files via upload

---
+++ DAY4/3_초기화5-1.cpp
@@ -0,0 +1,19 @@
+// 3_√ ±‚»≠5-1
+
+class Point
+{
+	int x, y;
+public:
+	Point(int a, int b) : x(a), y(b) {}
+};
+
+void f1(int   n) {}
+void f2(Point p) {}
+
+int main()
+{
+	int n1 = 3;
+
+	f1(3);
+}
+
