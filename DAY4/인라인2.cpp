e5dce8ff95d493d239989d560356dc88166e76f0
ecourse <smkang@codenuri.co.kr>
2022-06-23 11:56:10 +0900

    Add files via upload

--- DAY4/인라인2.cpp
+++ DAY4/인라인2.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <algorithm>
 
-void sort(int* x, int sz)
+void sort(int* x, int sz, bool(*cmp)(int, int) )
 {
 	for (int i = 0; i < sz - 1; i++)
 	{
 		for (int j = i + 1; j < sz; i++)
 		{
-			if (x[i] > x[j])
+//			if (x[i] > x[j])
+
+			if ( cmp(x[i], x[j]) )
 				std::swap(x[i], x[j]);
 		}
 	}
 }
+//-----------------------
+bool cmp1(int a, int b) { return a > b; }
+bool cmp2(int a, int b) { return a < b; }
+
 int main()
 {
 	int x[10] = { 1,3,5,7,9,2,4,6,8,10 };
