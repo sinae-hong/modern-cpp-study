d2bdf770f3ccfa0f7c80fc144086d6f190176d72
ecourse <smkang@codenuri.co.kr>
2022-06-23 15:44:59 +0900

    Add files via upload

--- DAY4/3_초기화5-1.cpp
+++ DAY4/3_초기화5-1.cpp
@@ -12,8 +12,19 @@ void f2(Point p) {}
 
 int main()
 {
-	int n1 = 3;
+	int n1 = { 3 };
 
-	f1(3);
+	f1(3);	// int n = 3
+	f1({ 3 });	// int n = {3}
+
+	f2({ 1,2 }); // ok.. Point pt = {1,2} 가 되므로
+
 }
+
+
+
+
+
+
+
 
