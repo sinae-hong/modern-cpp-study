8c057851cb4c6bb7b7db41f9d4924eeccd74141c
ecourse <smkang@codenuri.co.kr>
2022-06-24 16:06:13 +0900

    Add files via upload

--- DAY5/8_get2.cpp
+++ DAY5/8_get2.cpp
@@ -24,15 +24,24 @@ struct tuple<T, Types...> : public tuple<Types...>
 int main()
 {
 	//	tuple<> t0;
-	//	tuple<             short> t1; // short 한개 보관
-	//	tuple<     double, short> t2; // double 한개 보관
-	tuple<int, double, short> t3(1, 3.4, 2); // int 한개 보관
+	//	tuple<             short> t1;		// short 한개 보관   - 2
+	//	tuple<     double, short> t2;		// double 한개 보관  - 3.4
+	tuple<int, double, short> t3(1, 3.4, 2);// int 한개 보관     - 1
 
-	std::cout << t3.value << std::endl; // ?
+	std::cout << t3.value << std::endl; // 1
 
-	std::cout << t3.value << std::endl; // 3.4
+	std::cout << static_cast<tuple<double, short> &>(t3).value << std::endl; // 3.4
 
-	std::cout << t3.value << std::endl; // 2
+	std::cout << static_cast<tuple<short> &>(t3).value << std::endl; // 2
+
+
+	int n = get<0>(t3);
+}
+template<int N, typename TP>
+N번째 요소 타입&
+get(TP& tp)
+{
+	return static_cast<N번째기반클래스타입&>(tp).value;
 }
 
 
