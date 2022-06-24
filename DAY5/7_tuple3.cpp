154cf9d1350bc1da3ec961c4bfccf53a1f71c58a
ecourse <smkang@codenuri.co.kr>
2022-06-24 15:45:15 +0900

    Add files via upload

---
+++ DAY5/7_tuple3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+// Step2. 요소 한개 보관하기
+
+template<typename ... Types>
+struct tuple
+{
+	static constexpr int N = 0;
+};
+
+// tuple 사용시 타입을 한개이상 전달한 경우를 위한 부분 특수화
+template<typename T, typename ... Types>
+struct tuple<T, Types...>
+{
+	T value;
+
+	tuple() = default;
+	tuple(const T& v) : value(v) {}
+
+	static constexpr int N = 1;
+};
+
+int main()
+{
+	tuple<> t0;
+	tuple<short> t1;
+	tuple<double, short> t2;
+	tuple<int, double, short> t3;
+}
+
+
+
