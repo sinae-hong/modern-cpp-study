aa3ea0cdc36f86b904b3be0520d75025f4f7d0bd
ecourse <smkang@codenuri.co.kr>
2022-06-24 10:52:14 +0900

    Add files via upload

--- DAY5/3_enable_if1.cpp
+++ DAY5/3_enable_if1.cpp
@@ -22,10 +22,20 @@ int main()
 
 	// 정리하면
 	// 1. enable_if<true, 타입>::type 의 결과는 "타입"
-	// 2. enable_if<true>::type       의 결과는 "?"
+	// 2. enable_if<true>::type       의 결과는 "void"
 
-	// 3. enable_if<false, 타입>::type 의 결과는 ?
-	// 4. enable_if<false::type       의 결과는 ?
+	// 3. enable_if<false, 타입>::type 의 결과는 ?  "::type" 자체가 없다
+	// 4. enable_if<false>::type       의 결과는 ?  "::type" 자체가 없다
+}
+
+#include <type_traits> // 이 안에 "enable_if" 있습니다. C++ 표준입니다.
+
+template<typename T> void foo(T a)
+{
+	// T 에 의존해서 사용하면 typename 필요
+	typename std::enable_if<true, T>::type n1;
+
+	std::enable_if_t<true, T> n2;  // 이렇게 하면 위와 동일
 }
 
 
